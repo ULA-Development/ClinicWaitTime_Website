625b04f9204f4756b16b1e712b59584f2c42f8cd
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-05-31 18:32:24 -0400

    applying global colors to component

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <TextInput/>
       <Button onClick={() => console.log("click")}>Sign in </Button>
     </div>
   );
