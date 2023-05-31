092cf8ee1959d85aea9804f4bee9595268d56185
ethanmcf <ethan_mcfarland@outlook.com>
2023-05-31 09:58:44 -0400

    folder push

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import * as React from 'react'
+import { ReactComponent as InputIcon } from '../assets/envelope-solid.svg'
+import './TextInput.css'
 
 const TextInput = () => {
   return (
-    <div>textInput</div>
+    <div style={{bottom: 20}} className='text-field-shell'>
+      <input type="text" id='9' className='text-field'/>
+      <InputIcon className='input-icon'/>
+    </div>
   )
 }
+
+
 
 export default TextInput
