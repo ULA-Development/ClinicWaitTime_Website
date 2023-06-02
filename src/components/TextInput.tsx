3500336f206083e490760ce33a01967755eb0652
ethanmcf <ethan_mcfarland@outlook.com>
2023-06-02 10:12:15 -0400

    finished non-error design

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,35 +1,113 @@
-import React, { useState, useRef, Component } from "react";
-import { ReactComponent as InputIcon } from "../assets/icons/envelope-solid.svg";
+import React, { useState, useEffect } from "react";
+import { ReactComponent as EmailIcon } from "../assets/icons/envelope-solid.svg";
+import { ReactComponent as PasswordIcon } from "../assets/icons/lock-solid.svg";
+import { ReactComponent as NameIcon } from "../assets/icons/user-solid.svg";
+import { ReactComponent as EyeIcon } from "../assets/icons/eye-slash-solid.svg";
+import { ReactComponent as NPIIcon } from "../assets/icons/slack-hash.svg";
+import { ReactComponent as ReferIcon } from "../assets/icons/user-friends-solid.svg";
 import "./TextInput.css";
-import { set } from "firebase/database";
 
-function TextInput(){
-  const [focus, setFocus] = useState(false)
-  const [text, setText] = useState('')
+interface Props {
+  onChange: (arg0: string) => void;
+  value: string;
+  type: string;
+}
+
+const TextInput = ({ onChange, value, type }: Props) => {
+  const [focus, setFocus] = useState(false);
+  const [inputInfo, setInputInfo] = useState({
+    placeholder: "Email",
+    icon: <EmailIcon />,
+    type: "text",
+  });
+
+  useEffect(function loadData() {
+    switch (type) {
+      case "Name": {
+        setInputInfo({
+          placeholder: "Name",
+          icon: <NameIcon className="icon"/>,
+          type: "text",
+        });
+        break;
+      }
+      case "Password": {
+        setInputInfo({
+          placeholder: "Password",
+          icon: <PasswordIcon className="icon"/>,
+          type: "password",
+        });
+        break;
+      }
+      case "Confirm Password": {
+        setInputInfo({
+          placeholder: "Confirm password",
+          icon: <PasswordIcon className="icon"/>,
+          type: "password",
+        });
+        break;
+      }
+      case "NPI": {
+        setInputInfo({
+          placeholder: "NPI",
+          icon: <NPIIcon className="icon"/>,
+          type: "text",
+        });
+        break;
+      }
+      case "Referral": {
+        setInputInfo({
+          placeholder: "Referral code (optional)",
+          icon: <ReferIcon className="icon"/>,
+          type: "text",
+        });
+        break;
+      }
+      default: {
+        setInputInfo({
+          placeholder: "Email",
+          icon: <EmailIcon className="icon"/>,
+          type: "text",
+        });
+        break;
+      }
+    }
+  }, []);
 
   const handleFocus = () => {
+    console.log(value);
     // If text in field, don't loose focus
-    if(focus == true && text == ''){
-      setFocus(false)
-    }else{
-      setFocus(true)
+    if (focus == true && value == "") {
+      setFocus(false);
+    } else {
+      setFocus(true);
     }
-  }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    onChange(value);
+  };
 
   return (
-    <div onClick={handleFocus} className="container">
-      <input
-        value={text}
-        onBlur={handleFocus}
-        onChange={(input) => setText(input.target.value)}
-        type="text"
-        id="id"
-        className="input"
-      />
-      <div className={focus != true ? "background" : "background-focus"}>
-        <span className="label">Password</span>
+    <div className={focus != true ? "container" : "container focus"}>
+      <div className={focus != true ? "background" : "background focus"}>
+        <span className="label">{inputInfo.placeholder}</span>
       </div>
-      <InputIcon className="icon" />
+      {inputInfo.icon}
+      {inputInfo.type === "password" ? (
+        <EyeIcon className="visible-eye" />
+      ) : null}
+      <input
+        value={value}
+        onFocus={handleFocus}
+        onChange={handleChange}
+        onBlur={handleFocus}
+        type={inputInfo.type}
+        id={type}
+        className="input"
+        autoComplete="off"
+      />
     </div>
   );
 };
