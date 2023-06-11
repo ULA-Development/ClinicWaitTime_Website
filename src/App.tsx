3eabb6956eed26f8c8ad2ea91dbf75155d3c8848
ethanmcf <ethan_mcfarland@outlook.com>
2023-06-11 17:25:44 -0400

    Finishing sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import TextInput from "./components/TextInput";
 import Button from "./components/Button";
 import Dropdown from "./pages/Home/Dropdown";
-
+import Header from "./components/Header/Header";
+import SigninPage from "./pages/Signin/SigninPage"
 function App() {
+  
   return (
-    <div>
-      
-    </div>
+
+      <SigninPage/>
+
   );
 }
 
--- src/pages/Signin/SigninPage.tsx
+++ src/pages/Signin/SigninPage.tsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "../../components/Header/Header";
+import TextInput from "../../components/TextInput";
+import { ReactComponent as SigninIcon } from "../../assets/icons/sign-in-alt-solid.svg"
+import "./SigninPage.css";
 
-const index = () => {
+const SigninPage = () => {
+  const [emailText, setEmailText] = useState("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState<null | string>(
+    "Incorrect email"
+  );
+  const [passText, setPassText] = useState("");
+  const [passErrorMessage, setPassErrorMessage] = useState<null | string>(
+    "null"
+  );
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <Header />
+      <div className="content-container">
+        <div className="content">
+          <div className="title">
+            <SigninIcon className="logo"/>
+            <span className="title">Sign into ULA</span>
+          </div>
+          <span>Don't have an account? Create account</span>
+          <TextInput
+            value={emailText}
+            onChange={setEmailText}
+            type="Email"
+            errorMessage={emailErrorMessage}
+            setError={setEmailErrorMessage}
+          />
+          <TextInput
+            value={passText}
+            onChange={setPassText}
+            type="Password"
+            errorMessage={passErrorMessage}
+            setError={setPassErrorMessage}
+          />
+          <span>Forogt your password?</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default index
+export default SigninPage;
