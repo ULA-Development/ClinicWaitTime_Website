b3de96a3fb5fd48a9868017e22b24d9d01c58bc1
Ethan McFarland <89097702+ethanmcf@users.noreply.github.com>
2023-06-11 11:06:14 -0400

    Cleaning App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,7 @@ import Dropdown from "./components/Dropdown-2";
 
 function App() {
   return (
-    <div className="dropdown" style={{ width: "400px", height: "70px" }}>
-      <Dropdown
-        options={[
-          "Walk-In Clinic",
-          "Hospital",
-          "Specialist",
-          "Family Physician",
-        ]}
-      />
-    </div>
+   
   );
 }
 
