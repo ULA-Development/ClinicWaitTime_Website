9c459872bbad26417141ca1f05095c0a1a997852
ethanmcf <ethan_mcfarland@outlook.com>
2023-06-15 16:30:35 -0400

    Minor adjustments to Button UI

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,22 +5,11 @@ import "./Button.css";
 interface Props {
   children: ReactNode;
   onClick: () => void;
-  width?: number;
-  height?: number;
 }
 
-const Button = ({ children, onClick, width = 541.6, height = 76.5 }: Props) => {
+const Button = ({ children, onClick}: Props) => {
   const [clicked, setClicked] = useState(false);
   const buttonRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const button = buttonRef.current;
-    if (button) {
-      const buttonHeight = button.offsetHeight;
-      const fontSize = buttonHeight * 1; // Adjust the multiplier as needed
-      button.style.fontSize = `${fontSize}px`;
-    }
-  }, []);
   return (
     <button
       className={`Button ${clicked ? "Button-clicked" : ""}`}
@@ -29,16 +18,15 @@ const Button = ({ children, onClick, width = 541.6, height = 76.5 }: Props) => {
         setClicked(true);
       }}
       onAnimationEnd={() => setClicked(false)}
-      style={{ width: `${width}px`, height: `${height}px` }}
     >
       <div className="button-content" ref={buttonRef}>
         {children}{" "}
         <img
           src={icon}
           alt="Icon"
-          width={width / 15 + "px"}
-          height={height / 2.75 + "px"}
-          style={{ filter: "invert(100%)" }}
+          width={"20px"}
+          height={"20px"}
+          style={{ filter: "invert(100%)", marginLeft: "5px" }}
         />
       </div>
     </button>
--- src/pages/Signin/SigninPage.tsx
+++ src/pages/Signin/SigninPage.tsx
@@ -3,7 +3,7 @@ import Header from "../../components/Header/Header";
 import TextInput from "../../components/TextInput";
 import SmallFooter from "../../components/SmallFooter";
 import Button from "../../components/Button";
-import { ReactComponent as SigninIcon } from "../../assets/icons/sign-in-alt-solid.svg"
+import { ReactComponent as SigninIcon } from "../../assets/icons/sign-in-alt-solid.svg";
 import "./SigninPage.css";
 
 const SigninPage = () => {
@@ -12,42 +12,44 @@ const SigninPage = () => {
     null
   );
   const [passText, setPassText] = useState("");
-  const [passErrorMessage, setPassErrorMessage] = useState<null | string>(
-    null
-  );
+  const [passErrorMessage, setPassErrorMessage] = useState<null | string>(null);
   return (
     <div>
       <Header />
       <div className="content-container">
         <div className="content">
           <div className="title">
-            <SigninIcon className="sign-in-logo"/>
+            <SigninIcon className="sign-in-logo" />
             <span className="title">Sign into ULA</span>
           </div>
           <a href="/signup">Don't have an account? Create account</a>
-          <div className='input-fields'>
-          <TextInput
-            value={emailText}
-            onChange={setEmailText}
-            type="Email"
-            errorMessage={emailErrorMessage}
-            setError={setEmailErrorMessage}
-          />
-          <TextInput
-            value={passText}
-            onChange={setPassText}
-            type="Password"
-            errorMessage={passErrorMessage}
-            setError={setPassErrorMessage}
-          />
+          <div className="input-fields">
+            <TextInput
+              value={emailText}
+              onChange={setEmailText}
+              type="Email"
+              errorMessage={emailErrorMessage}
+              setError={setEmailErrorMessage}
+            />
+            <TextInput
+              value={passText}
+              onChange={setPassText}
+              type="Password"
+              errorMessage={passErrorMessage}
+              setError={setPassErrorMessage}
+            />
           </div>
           <a href="/forogotpassword">Forogt your password?</a>
-          <div style={{alignSelf: 'center'}}>
-          <Button width={450} height={50} onClick={() => console.log("sign in")}>Sign in</Button>
+          <div style={{ alignSelf: "center", width: "70%" }}>
+            <Button
+              onClick={() => console.log("sign in")}
+            >
+              Sign in
+            </Button>
           </div>
         </div>
       </div>
-      <SmallFooter/>
+      <SmallFooter />
     </div>
   );
 };
