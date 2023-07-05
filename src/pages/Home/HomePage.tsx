e0ca56781fb9af748606f9ad2dfe9fd8d2f3f9a1
ethanmcf <ethan_mcfarland@outlook.com>
2023-07-05 13:06:10 -0400

    Changed output result

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -5,23 +5,23 @@ import axios from "axios";
 
 const Index: React.FC = () => {
   const handleClick = () => {
-  //   navigator.geolocation.getCurrentPosition((position: GeolocationPosition) => {
-  //     console.log(position);
-  // });
         navigator.geolocation.getCurrentPosition(async (position: GeolocationPosition) => {
           const { latitude, longitude } = position.coords;
-
           try {
             const apiKey = "qnqs8KIVHizEGZrlrFdR--I0Run_DM5H5qQjoOd87NQ";
             const apiUrl = `https://revgeocode.search.hereapi.com/v1/revgeocode?apiKey=${apiKey}&at=${latitude},${longitude}`;
       
-            const response = await axios.get(apiUrl);
-            console.log(response.data);
-
-            const address = response.data.items[0].address.label;
-            console.log(address);
+            const response = (await axios.get(apiUrl)).data.items[0];
+            const currInfo = {
+              location: {
+                lat: response.position.lat,
+                lng: response.position.lng
+              },
+              address: response.address.label
+            }
+            console.log(currInfo)
           } catch (error) {
-            console.error("Error:", error);
+            alert(error);
           }
         });
   };
