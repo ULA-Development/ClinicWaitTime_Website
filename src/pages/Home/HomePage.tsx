ff8b7c07d79bcf2bd5d8f715d41f263b71e3a921
ethanmcf <ethan_mcfarland@outlook.com>
2023-07-05 13:09:26 -0400

    Style change

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,5 +1,4 @@
-
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import Header from "../../components/Header/Header";
 import { ReactComponent as Location } from "../../assets/icons/location-crosshairs-solid.svg";
 import axios from "axios";
@@ -7,40 +6,42 @@ import { dbHandler } from "../../data/firebase";
 import { useSelector } from "react-redux";
 
 const HomePage = () => {
-  const isMobile = useSelector((state: any) => state.isMobile.value)
+  const isMobile = useSelector((state: any) => state.isMobile.value);
   useEffect(() => {
-    dbHandler.fetchClinics()
-  }, [])
+    dbHandler.fetchClinics();
+  }, []);
   const handleClick = () => {
-        navigator.geolocation.getCurrentPosition(async (position: GeolocationPosition) => {
-          const { latitude, longitude } = position.coords;
-          try {
-            const apiKey = "qnqs8KIVHizEGZrlrFdR--I0Run_DM5H5qQjoOd87NQ";
-            const apiUrl = `https://revgeocode.search.hereapi.com/v1/revgeocode?apiKey=${apiKey}&at=${latitude},${longitude}`;
-      
-            const response = (await axios.get(apiUrl)).data.items[0];
-            const currInfo = {
-              location: {
-                lat: response.position.lat,
-                lng: response.position.lng
-              },
-              address: response.address.label
-            }
-            console.log(currInfo)
-          } catch (error) {
-            alert(error);
-          }
-        });
+    navigator.geolocation.getCurrentPosition(
+      async (position: GeolocationPosition) => {
+        const { latitude, longitude } = position.coords;
+        try {
+          const apiKey = "qnqs8KIVHizEGZrlrFdR--I0Run_DM5H5qQjoOd87NQ";
+          const apiUrl = `https://revgeocode.search.hereapi.com/v1/revgeocode?apiKey=${apiKey}&at=${latitude},${longitude}`;
+
+          const response = (await axios.get(apiUrl)).data.items[0];
+          const currInfo = {
+            location: {
+              lat: response.position.lat,
+              lng: response.position.lng,
+            },
+            address: response.address.label,
+          };
+          console.log(currInfo);
+        } catch (error) {
+          alert(error);
+        }
+      }
+    );
   };
   return (
     <div>
-      <Location onClick={() => handleClick()} 
-      style={{ width: "50px", height: "50px" }}/>
+      <Location
+        onClick={() => handleClick()}
+        style={{ width: "50px", height: "50px" }}
+      />
       {isMobile ? <p>Mobile</p> : <p>Desktop</p>}
     </div>
   );
 };
 
-
 export default HomePage;
-
