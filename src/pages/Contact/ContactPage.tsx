2b2db549291512fafdc069adb7b8b21fd53347e6
kuzeyozturac <kuzeyozturac2@gmail.com>
2023-07-14 18:36:08 +0300

    Amogus

--- src/pages/Contact/ContactPage.tsx
+++ src/pages/Contact/ContactPage.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import IconSvg from "../Home/IconSvg";
+import GreenIcon from "../../assets/icons/svgs/g1.svg";
 
 const index = () => {
   return (
     <div>
-      <Header selectedItem={"Home"} />
-      Contact
+      <GreenIcon />
     </div>
   );
 };
--- src/pages/Home/IconSvg.tsx
+++ src/pages/Home/IconSvg.tsx
@@ -20,12 +20,14 @@ const IconSvg: React.FC<IconSvgProps> = ({ business, number }) => {
   const iconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" width="40" height="60"><path fill="${color}" d="M172.268 501.67C26.97 291.031 0 269.413 0 192 0 85.961 85.961 0 192 0s192 85.961 192 192c0 77.413-26.97 99.031-172.268 309.67-9.535 13.774-29.93 13.773-39.464 0z"/></svg>`;
 
   return (
-    <div className="icon-svg">
-      <div className="icon-circle">
-        <span className="icon-number">{number}</span>
+    <svg>
+      <div className="icon-svg">
+        <div className="icon-circle">
+          <span className="icon-number">{number}</span>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: iconSvg }} />
       </div>
-      <div dangerouslySetInnerHTML={{ __html: iconSvg }} />
-    </div>
+    </svg>
   );
 };
 
